deff10731fd6525e3d0eefcb829f4cb76f2da4e9
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2023-12-07 22:48:54 -0500

    cartas

--- juego.js
+++ juego.js
@@ -18,12 +18,15 @@ this.valor = valor;
 
     }
 
-let jugador1 = new Juego("Player1",3,Number=prompt("Ingrese un numero del 1 a 7"));
+let jugador1 = new Juego("Player1",3,Number(prompt("Ingrese un numero del 1 a 7")));
+let jugador2 = new Juego("Player2",3,Number(prompt("Ingrese un numero del 1 a 7")));
 
 
-let etiquetaplayerOne = document.getElementById("nickplayerone");
-etiquetaplayerOne.innerHTML = `Player: ${jugador1.apodo}`;
-let etiquetaplayerOnevidas = document.getElementById("nlifesplayerone");
-etiquetaplayerOnevidas.innerHTML = `Vidas: ${jugador1.nvidas}`;
-let etiquetaplayerOnevalor = document.getElementById("valueplayerone");
-etiquetaplayerOnevalor.innerHTML = `Valor de la carta: ${jugador1.valor}`;
+let etiquetaplayerOne=document.getElementById("nickplayerone").innerHTML = `Player: ${jugador1.apodo}`;
+let etiquetaplayerOnevidas=document.getElementById("nlifesplayerone").innerHTML = `Vidas: ${jugador1.nvidas}`;
+let etiquetaplayerOnevalor=document.getElementById("valueplayerone").innerHTML = `Valor de la carta: ${jugador1.valor}`;
+
+let etiquetaplayerTwo=document.getElementById("nickplayertwo").innerHTML = `Player: ${jugador2.apodo}`;
+let etiquetaplayerTwovidas=document.getElementById("nlifesplayertwo").innerHTML = `Vidas: ${jugador2.nvidas}`;
+let etiquetaplayerTwovalor=document.getElementById("valueplayertwo").innerHTML = `Valor de la carta: ${jugador2.valor}`;
+
